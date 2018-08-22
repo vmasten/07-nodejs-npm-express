53d69a20854035e2d9c8db76b4e2503c14536b0e
Vince Masten <vmasten@me.com>
2018-08-22 09:30:34 -0700

    initial commit

---
+++ vince-hannah/server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+
+// REVIEW: POST route needs to parse the body passed in with the request.
+// POST middleware
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/articles', (request, response) => {
+  // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
+  console.log(request.body);
+  response.status(201).json(request.body);
+})
