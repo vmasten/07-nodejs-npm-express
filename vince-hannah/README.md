5f2815ea49ebb986e06f53ec36e52937338c3947
Hannah Sindorf <hannah@hsindorf.com>
2018-08-22 10:18:48 -0700

    finshed server based on requirements

---
+++ vince-hannah/README.md
@@ -0,0 +1,37 @@
+# 07 Node.js npm Express
+
+**Author**: Vince and Hannah
+**Version**: 1.0.0
+
+## Overview
+This is an Express server that serves a blog on port 3000. 
+
+## Getting Started
+1. Ensure node.js is installed
+1. Clone the repository
+1. In the project folder, in the terminal run "npm install express"
+1. Run "node server" to start the server
+1. Visit 127.0.0.1:3000 in your browser. You can also visit 127.0.0.1:3000/new to see the interface to write a new post.
+
+## Architecture
+
+### For page design:
+- HTML
+- CSS
+- JavaScript
+- jQuery
+- Handlebars.js
+- Highlight.js
+- Marked.js
+
+### For server:
+- Express.js
+- Node.js
+
+## Change Log
+- 8/22/18: Initial release
+
+## Additional Resources Used
+- [https://expressjs.com/en/api.html](https://expressjs.com/en/api.html)
+- [https://expressjs.com/en/starter/faq.html](https://expressjs.com/en/starter/faq.html)
+- [https://scotch.io/tutorials/use-expressjs-to-deliver-html-files](https://scotch.io/tutorials/use-expressjs-to-deliver-html-files)
--- vince-hannah/server.js
+++ vince-hannah/server.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 
+const path = require('path');
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -9,14 +11,24 @@ const PORT = process.env.PORT || 3000;
 // REVIEW: POST route needs to parse the body passed in with the request.
 // POST middleware
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static('./public'));
 //Our files are in a public directory now so that we only expose the files we want users to download. Express.js serves our local files by first checking the public directory for a match to the request, then it continues on to the below instructions if need be
+
+app.get('/new', (request, response) => {
+  response.sendFile(path.join(__dirname + '/public/new.html'));
+});
+
 app.post('/articles', (request, response) => {
   // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
   console.log(request.body);
   response.status(201).json(request.body);
 });
 
-app.listen(PORT, function() {
+app.listen(PORT, () => {
   console.log('listening on port', PORT);
+});
+
+app.use((request, response) => {
+  response.status(404).send('Try again later');
 });
